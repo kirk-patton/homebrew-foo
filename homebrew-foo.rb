8cf6d1854847d851fe09ac7f2324ba97ba625b49
Kirk Patton <kpatton@verizonmedia.com>
2022-02-26 08:08:09 -0800

    first formula

---
+++ homebrew-foo.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class HomebrewFoo < Formula
+  desc "Explore setting up a homebrew formula"
+  homepage "https://github.com/kirk-patton/homebrew-foo/tree/foo"
+  url "https://github.com/kirk-patton/homebrew-foo/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "6b12e6d98aadd4d7d37027f0cb4566eb668833a87b2ce85d1ee58fb8d5e7c05b"
+  version "v0.1.0"
+  license "public :public_domain"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    dir.install foo.sh
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test homebrew-foo`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
